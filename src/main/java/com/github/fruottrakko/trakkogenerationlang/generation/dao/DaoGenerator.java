2dd5533ea639f740ed6954e79319572cd107a36e
JannesForTec <jannes@opicht.de>
2020-02-22 00:53:18 +0100

    update .gitignore and add source file header template

--- src/main/java/com/github/fruottrakko/trakkogenerationlang/generation/dao/DaoGenerator.java
+++ src/main/java/com/github/fruottrakko/trakkogenerationlang/generation/dao/DaoGenerator.java
@@ -7,8 +7,8 @@ import com.github.fruottrakko.trakkogenerationlang.files.tgl.TglFile;
 public class DaoGenerator {
 
     //Generate Dirs: filename.[filename.java|dao.[filenameDao.java|<dbname>.filenameDao<dbname>.java]]
-    //Generate Model source file
-    //Generate Dao Interface source file
+    //Generate Model source file                -> Copy header into source file
+    //Generate Dao Interface source file        -> Copy header into source file
     //Generate Database specific Dao Implmentations (type-atlas (v.2?))
 
     public static void generateDaoFromTgl(TglFile tglFile, Path destinationPath) {
